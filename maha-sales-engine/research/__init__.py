ac39babf294526ee06761f6f726c050faee9731b
prahlad168 <65000711+prahlad168@users.noreply.github.com>
2026-08-30 22:49:56 +0800

    feat(research): expose dynamic web research provider

--- maha-sales-engine/research/__init__.py
+++ maha-sales-engine/research/__init__.py
@@ -1,5 +1,9 @@
-"""Research adapters and curated research inputs."""
+"""MAHA research adapters."""
 
+from .web_search import DuckDuckGoResearchProvider, WebResearchError, discover_bali_businesses
 from .bali import REAL_BALI_LEADS, get_bali_research_leads
 
-__all__ = ["REAL_BALI_LEADS", "get_bali_research_leads"]
+__all__ = [
+    "DuckDuckGoResearchProvider", "WebResearchError", "discover_bali_businesses",
+    "REAL_BALI_LEADS", "get_bali_research_leads",
+]
